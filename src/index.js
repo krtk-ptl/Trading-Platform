46ba313d1173738b3ab80b7f6cb90e446293cc1e
Kartik Patil <111671355+kartikp06@users.noreply.github.com>
2025-07-25 15:33:29 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landing_page/home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>no
-      <hl>Hello</hl>
-    </div>
+    <HomePage />
   </React.StrictMode>
 )
---
+++ src/landing_page/home/Education.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Education() {
+  return (  
+    <h1>Educatioon</h1>
+  );
+}
+
+export default Education;
